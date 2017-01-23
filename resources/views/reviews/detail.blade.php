47c2c2b69bb820b3fa945c4aa52656a94365bfca
lmcowling <lmcowling@mail.com>
2017-01-23 18:13:27 +0000

    Commit after login auth was not included on review deletion

--- resources/views/reviews/detail.blade.php
+++ resources/views/reviews/detail.blade.php
@@ -5,9 +5,13 @@
   </div>
   <div class="pageBody">
     <h3>{{ $review->body }}</h3>
+    @if (Auth::guest())
+      <h4>Login to delte a review</h4>
+    @else
     <form method="POST" action="/reviews/{{ $review->id }}/delete">
       <button type="submit">Delete review</button>
       <input type="hidden" name="_token" value="{{ csrf_token() }}">
     </form>
+    @endif
   </div>
 @endsection
